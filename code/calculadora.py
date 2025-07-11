43a2079645851d35b956b5bbcb28c9329ad3a89c
Gustavo Nucci <gustavov.nucci@gmail.com>
2025-07-11 01:00:31 -0300

    chore: Repo initialized

---
+++ code/calculadora.py
@@ -0,0 +1,18 @@
+class Calculadora:
+    """Classe para realizar operações matemáticas básicas."""
+    
+    @staticmethod
+    def soma(a,b):
+        return a+b
+
+    @staticmethod
+    def subtracao(a,b):
+        return a-b
+    
+    @staticmethod
+    def multiplicacao(a,b):
+        return a*b
+    
+    @staticmethod
+    def divisao(a,b):
+        return a/b
